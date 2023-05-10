88e375210043a7f0d228c90b082f6a6775c23ec8
jbk-xiao <229088026@qq.com>
2023-05-10 17:20:44 +0800

    modify gen_tweet_sequences.py.

--- twibot-20/preprocess/gen_tweet_sequences.py
+++ twibot-20/preprocess/gen_tweet_sequences.py
@@ -1,10 +1,11 @@
 import json
+
+import gensim.utils
 import ijson
 import numpy as np
 import pandas
 from nltk import tokenize
 from tqdm import tqdm
-import jieba
 
 from nltk.corpus import stopwords
 from sklearn.feature_extraction import _stop_words as stop_words
@@ -16,6 +17,21 @@ tmp_files_root = r"./tmp-files"
 
 
 def get_tweet_corpus():
+    fb = open(rf'{tmp_files_root}/corpus.txt', 'w')
+    with open(rf'{datasets_root}/node.json') as f:
+        data = ijson.items(f, 'item')
+        for item in tqdm(data, ncols=0):
+            if item['id'].find('t') == -1:
+                continue
+            if item['text'] is None:
+                continue
+            line = item['text']
+            fb.write(line + '\n')
+    fb.close()
+
+
+def trim_rule(word: str, count: int, min_count: int) -> int:
+
     nltk_stopwords = set(stopwords.words('english'))
     sklearn_stopwords = stop_words.ENGLISH_STOP_WORDS
     all_stopwords = set()
@@ -26,36 +42,24 @@ def get_tweet_corpus():
     all_stopwords.add('https')
     all_stopwords.add('http')
 
-    def is_allowed(word: str) -> bool:
-        if word.find('@') != -1 or word.find('#') != -1:
-            return False
-        if len(word) > 15 or len(word) == 0:
-            return False
-        # if word is not unicode? if word is not start with alpha or number?
-        if not ('a' <= word[0] <= 'z' or 'A' <= word[0] <= 'Z' or '0' <= word[0] <= '9'):
-            return False
-        if word in all_stopwords:
-            return False
-        return True
-    fb = open(rf'{tmp_files_root}/corpus.txt', 'w')
-    with open(rf'{datasets_root}/node.json') as f:
-        data = ijson.items(f, 'item')
-        for item in tqdm(data, ncols=0):
-            if item['id'].find('t') == -1:
-                continue
-            if item['text'] is None:
-                continue
-            line = item['text']
-            word_list = [word for word in jieba.cut(line) if is_allowed(word)]
-            line = ' '.join(word_list)
-            fb.write(line + '\n')
-    fb.close()
+    if count < min_count:
+        return gensim.utils.RULE_DISCARD
+    if word.find('@') != -1 or word.find('#') != -1:
+        return gensim.utils.RULE_DISCARD
+    if len(word) > 15 or len(word) == 0:
+        return gensim.utils.RULE_DISCARD
+    # if word is not unicode? if word is not start with alpha or number?
+    if not ('a' <= word[0] <= 'z' or 'A' <= word[0] <= 'Z' or '0' <= word[0] <= '9'):
+        return gensim.utils.RULE_DISCARD
+    if word in all_stopwords:
+        return gensim.utils.RULE_DISCARD
+    return gensim.utils.RULE_KEEP
 
 
 def get_word2vec_model():
     sentences = word2vec.Text8Corpus(rf'{tmp_files_root}/corpus.txt')
     print('Training word2vec model...')
-    model = word2vec.Word2Vec(sentences, vector_size=128, workers=4, min_count=10)
+    model = word2vec.Word2Vec(sentences, vector_size=128, workers=4, trim_rule=trim_rule, min_count=10)
     vectors = model.wv.vectors
     key_to_index = model.wv.key_to_index
     index_to_key = model.wv.index_to_key
